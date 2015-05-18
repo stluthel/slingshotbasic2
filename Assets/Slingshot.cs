3d023ce011cf5dd5a7c90e395d60031c50543686
Lukas Thelen <stluthel@stud.h-da.de>
2015-05-18 16:02:25 +0200

    Slingshot added with mouse behavior.

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	public GameObject launchPoint;
+
+	void Awake() {
+		Transform launchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+}
+
+	void OnMouseEnter() {
+		//print ("Yay, the Mouse Works!");
+		launchPoint.SetActive (true);
+	}
+
+	void OnMouseExit() {
+		//print ("Whoops! The Mouse has exited!");
+		launchPoint.SetActive (false);
+	}
+}
